6ece0719f2e6a0e6e565ae5c6720b8b27aab82c9
Justin <j.hebenstreit25@gmail.com>
2025-10-29 11:37:51 -0500

    updated images for portfolio sites

--- client/src/components/Portfolio/PortfolioData.tsx
+++ client/src/components/Portfolio/PortfolioData.tsx
@@ -1,10 +1,10 @@
 import { Images } from '@/assets/images/images';
 
 export interface IPortfolioCard {
-    image: string;
+    image?: string;
     name: string;
     description: string;
-    gitHubRepo: string;
+    gitHubRepo?: string;
     deployment?: string;
     tags: string[];                        // NEW
     group?: 'Recent Projects' | 'Early Projects';  // NEW (for section titles)
@@ -12,28 +12,13 @@ export interface IPortfolioCard {
 
 const portfolioData: IPortfolioCard[] = [
     {
-        image: Images.Asphalt,
-        name: 'Asphalt Legends v1',
-        description: 'Non-React version of Asphalt Legends Unite video game site',
-        gitHubRepo: 'https://github.com/JHebenstreit48/asphalt-legends-unite',
-        deployment: 'https://jhebenstreit48.github.io/asphalt-legends-unite/',
+        image: Images.StarWars,
+        name: 'Star Wars v1',
+        description: 'Non-React version of Star Wars website',
+        gitHubRepo: 'https://github.com/JHebenstreit48/star-wars',
+        deployment: 'https://jhebenstreit48.github.io/star-wars/',
         tags: []
     },
-    {
-        image: Images.CodeQuest,
-        name: 'DevScriptStax',
-        description: 'React version of Coding Notes website',
-        gitHubRepo: 'https://github.com/JHebenstreit48/DevScriptStax',
-        deployment: 'https://devscriptstax.netlify.app/',
-        tags: []
-    },
-    // {
-    //     image: Images.StarWars,
-    //     name: 'Star Wars v1',
-    //     description: 'Non-React version of Star Wars website',
-    //     gitHubRepo: 'https://github.com/JHebenstreit48/star-wars',
-    //     deployment: 'https://jhebenstreit48.github.io/star-wars/'
-    // },
     {
         image: Images.CodingNotesV1,
         name: 'Portfolio v1',
@@ -43,11 +28,27 @@ const portfolioData: IPortfolioCard[] = [
         tags: []
     },
     {
+        image: Images.AsphaltLegendsv1,
+        name: 'Asphalt Legends v1',
+        description: 'Non-React version of Asphalt Legends Unite video game site',
+        gitHubRepo: 'https://github.com/JHebenstreit48/asphalt-legends-unite',
+        deployment: 'https://jhebenstreit48.github.io/asphalt-legends-unite/',
+        tags: []
+    },
+    {
         image: Images.ALUTracker,
         name: 'ALU Tracker',
         description: 'React version of Asphalt Legends Unite video game site',
         gitHubRepo: 'https://github.com/JHebenstreit48/asphalt-legends-unite-react',
         deployment: 'https://asphaltlegendsunitetracker.netlify.app/',
+        tags: []
+    },
+    {
+        image: Images.DevScriptStax,
+        name: 'DevScriptStax',
+        description: 'React version of Coding Notes website',
+        gitHubRepo: 'https://github.com/JHebenstreit48/DevScriptStax',
+        deployment: 'https://devscriptstax.netlify.app/',
         tags: []
     },
     {
@@ -64,6 +65,22 @@ const portfolioData: IPortfolioCard[] = [
         description: "A web application for my notes on all things console gaming related.",
         gitHubRepo: 'https://github.com/JHebenstreit48/PixelProse',
         deployment: 'https://pixelprose.netlify.app/',
+        tags: []
+    },
+    {
+        image: Images.AppFoundry,
+        name: 'App Foundry',
+        description: 'A web application for my notes on all things mobile app and game development.',
+        gitHubRepo: 'https://github.com/JHebenstreit48/AppFoundry',
+        deployment: 'https://appfoundry.netlify.app/',
+        tags: []
+    },
+    {
+        // image: Images.Film2Bytes,
+        name: 'Film2Bytes',
+        description: 'A web application for my film scanning and retouch services business.',
+        gitHubRepo: '',
+        deployment: '',
         tags: []
     }
 ];
